8584301608c0bef7c3c0d72ab2322a19d6d9f172
prathamthe1st <prathamg1803@gmail.com>
2023-07-08 16:14:39 +0530

    Excel App started

---
+++ backend/project/excel_impex/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ExcelImpexConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'excel_impex'
--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'user_handler',
+    'excel_impex',
 ]
 
 MIDDLEWARE = [
